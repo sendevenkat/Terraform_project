60d68d0e3f44895d3338d5dfe84f3a855967a299
venkat <venkat@example.com>
2026-06-03 23:54:13 +0530

    updated data block in storage account

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,6 +1,11 @@
+data "azurerm_resource_group" "Rg-dev" {
+  name = "Rg-terraform"
+  
+}
+
 resource "azurerm_storage_account" "dev-storage" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = data.azurerm_resource_group.Rg-dev
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.replication_type
